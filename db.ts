c6f0b8edc4317f7e3e3d5b7861eb2b19993afb57
Jeyadev <73011515+Jeyadev-Muthukumarasamy@users.noreply.github.com>
2025-01-09 10:35:11 +0530

    Update db.ts

--- db.ts
+++ db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectToDb = async () => {
   try {
-    const response = await mongoose.connect("mongodb+srv://jeydev007:295SFr3UdS6jKdkU@cluster0.5u57x.mongodb.net/");
+    const response = await mongoose.connect("mongodb+srv://jeydev007:uWGTs9KCMWRrcpCO@cluster0.5u57x.mongodb.net/");
     console.log("Connected to DB", response);
   } catch (error) {
     if (error instanceof Error) {
